6f56fafd6e4b0245d6717d9f2c9f748d15b9af1a
Larson-Dav <larsonkapita@gmail.com>
2024-02-26 10:58:07 +0200

    ReOrganize tweet-Action Components

--- src/Components/tweetComponents/tweetActions.jsx
+++ src/Components/tweetComponents/tweetActions.jsx
@@ -7,13 +7,22 @@ import { ActionItem } from './tweetActionItems'
 
 
 export function TweetActions({Action}){
+    const actionComponent = [
+        {img: msgImg, msg: Action.msg, text: true}, 
+        {img: replieImg, msg: Action.replie, text: true}, 
+        {img: likeImg, msg: Action.like, text: true}, 
+        {img: uploadImg, msg: null, text: false}
+    ]
     return (
         <>
            <div className='tweet-actions'>
-                <ActionItem icon={msgImg} msg={Action.msg} hasText={true}></ActionItem>
-                <ActionItem icon={replieImg} msg={Action.replie} hasText={true}></ActionItem>
-                <ActionItem icon={likeImg} msg={Action.like} hasText={true}></ActionItem>
-                <ActionItem icon={uploadImg} msg={null} hasText={false}></ActionItem>
+            {
+                actionComponent.map(function(action){
+                    return <> 
+                        <ActionItem icon={action.img} msg={action.msg} hasText={action.text}></ActionItem>
+                    </>
+                })
+            }
            </div>
         </>
     )
